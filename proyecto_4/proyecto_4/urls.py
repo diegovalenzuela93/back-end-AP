ec00c830316bbfaf941f77809026726bbec32189
diegovalenzuela93 <diegova2793@gmail.com>
2023-10-26 11:13:47 -0300

    clase 25/10

--- proyecto_4/proyecto_4/urls.py
+++ proyecto_4/proyecto_4/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for proyecto3 project.
+URL configuration for proyecto_4 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from gestion_app import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('busqueda/', views.busqueda),
-    path('buscar/', views.busca)
 ]
