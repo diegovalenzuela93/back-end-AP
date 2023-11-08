159994ff9aa0e8882ec598c9646fbf2026294c18
diegovalenzuela93 <diegova2793@gmail.com>
2023-11-08 20:08:59 -0300

    avance 08/11

--- proyecto_4/proyecto_4_app/views.py
+++ proyecto_4/proyecto_4_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from proyecto_4_app.models import empleados 
 
 # Create your views here.
+def listaempleados(request):
+    emplea = empleados.objects.all()
+    data = {'emple' : emplea}
+    
+    return render(request, 'empleados.html', data)
